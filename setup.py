d3a53c724652fa58751d6e6114bef91239445603
Joerg Schnitzbauer <joschnitzbauer@gmail.com>
2017-10-09 06:46:47 +0000

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(name='dalymi',
+      version=0.1,
+      author='Joerg Schnitzbauer',
+      author_email='joschnitzbauer@gmail.com',
+      license='MIT',
+      packages=['dalymi'])
