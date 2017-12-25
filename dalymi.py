48a990b96ed8a57ac5a4ec89640deae74cdb2741
Joerg Schnitzbauer <joschnitzbauer@gmail.com>
2017-12-25 22:36:06 +0000

    no force option

--- dalymi.py
+++ dalymi.py
@@ -102,8 +102,6 @@ class Pipeline:
             missing = [_ for _ in output if not _.check(context)]
             if missing:
                 self.log(f'Missing outputs {missing} of function <{func.__name__}>.', context)
-            elif context['force']:
-                self.log(f'Force-running function <{func.__name__}>.', context)
             else:
                 self.log(f'Skipping function <{func.__name__}>, because all outputs exist.', context)
                 return
@@ -176,10 +174,9 @@ class Pipeline:
         graph.edges(self.consumers)
         graph.render(**kwargs)
 
-    def run(self, task=None, execution_date=pd.Timestamp('today').date(), force=False, verbose=False, **context):
+    def run(self, task=None, execution_date=pd.Timestamp('today').date(), verbose=False, **context):
         context['task'] = task
         context['execution_date'] = execution_date
-        context['force'] = force
         context['verbose'] = verbose
         pretty_context = pprint.pformat(context)
         pretty_indented_context = '\n'.join(['  ' + _ for _ in pretty_context.split('\n')])
@@ -206,8 +203,6 @@ class PipelineCLI():
         self.run_parser.add_argument('-t', '--task', help='run a specific task')
         self.run_parser.add_argument('-e', '--execution-date', default=pd.Timestamp('today').date(),
                                      type=lambda x: pd.to_datetime(x).date(), help='the date of execution')
-        self.run_parser.add_argument('-f', '--force', action='store_true',
-                                     help='force task to run even if its output already exists')
         self.run_parser.add_argument('-v', '--verbose', action='store_true', help='be verbose about pipeline internals')
 
         self.plot_parser = self.subparsers.add_parser('plot', help='plot the DAG')
