8219aa3818864b2a17f9aa13b4180920089c59e2
Joerg Schnitzbauer <joschnitzbauer@gmail.com>
2018-01-31 08:25:13 +0100

    nicer description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(author='Joerg Schnitzbauer',
       author_email='joschnitzbauer@gmail.com',
-      description='A lightweight, data-focused and non-opinionated pipeline manager written in and for Python.',
+      description='[data like you mean it] A lightweight, data-focused and non-opinionated pipeline manager written in and for Python.',
       license='MIT',
       name='dalymi',
       packages=['dalymi'],
