a5129ae62c2e8bb7b69c079b92b6ef65dc9e66d4
Jörg Schnitzbauer <jorg.schnitzbauer@ikea.com>
2019-11-21 23:12:26 +0100

    add long description for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 
 setup(author='Joerg Schnitzbauer',
       author_email='joschnitzbauer@gmail.com',
       description=('[data like you mean it] A lightweight, data-focused and non-opinionated pipeline manager written '
                    'in and for Python.'),
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       license='MIT',
       name='dalymi',
       packages=['dalymi'],
